be39c89ff6b9be87c6b4c3188a57c818eae5cea6
Roshan Pandey <pandeyroshan556@gmail.com>
2022-02-03 15:11:39 +0530

    Minor Tweaks

--- src/main/java/in/rakuten/b2bcommerce/controller/ProductController.java
+++ src/main/java/in/rakuten/b2bcommerce/controller/ProductController.java
@@ -3,7 +3,6 @@ package in.rakuten.b2bcommerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
